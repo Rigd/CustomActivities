419442924b3700faaad712ba390992eedeaa865b
Mike Dimitroff <mpd@hexia.org>
2018-10-26 14:07:46 -0700

    refactoring

--- zoom/src/lambda_function.py
+++ zoom/src/lambda_function.py
@@ -10,10 +10,11 @@ import jwt
 
 API_KEY = os.environ.get("ZOOM_API_KEY")
 API_SECRET = os.environ.get("ZOOM_API_SECRET")
-API_USER_ID = os.environ.get("ZOOM_UID") #e-mail address of a Zoom user
+API_USER_ID = os.environ.get("ZOOM_UID")  # e-mail address of a Zoom user
 
 INSTANT_MEETING = 1
 SCHEDULED_MEETING = 2
+
 
 def get_jwt_token(ttl_sec=60):
     """JWT token from API KEY.  Zoom API v2 uses JWT for security"""
@@ -26,6 +27,7 @@ def get_jwt_token(ttl_sec=60):
         "exp": int(time() + ttl_sec)
     }
     return jwt.encode(payload, API_SECRET, algorithm='HS256', headers=header).decode()
+
 
 MSG_SUCCESS = [
     {"mrkdwn_in": ["text",
@@ -49,12 +51,13 @@ MSG_SUCCESS = [
     }
 ]
 
+
 def slack_msg_success(url, mid):
     """fill the slack message template with url and meeting ID"""
     msg = MSG_SUCCESS
     msg[0]['actions'][0]['url'] = url
     msg[0]['fallback'] = "Join meeting: %s" % url
-    msg[0]['fields'][0]['value'] = 'Meeting ID: %d'%mid
+    msg[0]['fields'][0]['value'] = 'Meeting ID: %d' % mid
     return msg
 
 
@@ -78,7 +81,8 @@ def slack_msg_failure(error_msg):
     msg[0]['text'] = error_msg
     return msg
 
-def lambda_handler(event, context):
+
+def lambda_handler(event):
     """The main entry point of the lambda module.
     Gets called when your custom activity needs fulfillment"""
 
@@ -94,11 +98,12 @@ def lambda_handler(event, context):
         return default
 
     if API_KEY is None or API_SECRET is None or API_USER_ID is None:
-        slack_msg = slack_msg_failure("One or more credentials are missing.  Please, ensure "\
-                "that ZOOM_API_KEY, ZOOM_UID, and ZOOM_API_SECRET are set properly.")
+        slack_msg = slack_msg_failure("One or more credentials are missing.  Please, ensure "
+                                      "that ZOOM_API_KEY, ZOOM_UID, and "
+                                      "ZOOM_API_SECRET are set properly.")
     else:
         settings = {
-            'join_before_host':set_value('join_before_host', False),
+            'join_before_host': set_value('join_before_host', False),
             'host_video': set_value('host_video', False),
             'participant_video': set_value('participant_video', False),
             'mute_upon_entry': set_value('mute_upon_entry', False),
@@ -115,18 +120,18 @@ def lambda_handler(event, context):
         req = {
             "topic": event['topic'] if 'topic' in event else 'RigD-created meeting',
             "type": SCHEDULED_MEETING,
-            "start_time": strftime("%Y-%m-%d'T'%H:%M:%S'Z'", gmtime()), #start now
-            "timezone":'UTC',
-            "duration":set_value('duration', 60), #minutes
+            "start_time": strftime("%Y-%m-%d'T'%H:%M:%S'Z'", gmtime()),  # start now
+            "timezone": 'UTC',
+            "duration": set_value('duration', 60),  # minutes
             "password": event['password'] if 'password' in event else None,
-            "agenda": event['agenda'] if 'agenda' in event else 'Item 1: Playing '\
+            "agenda": event['agenda'] if 'agenda' in event else 'Item 1: Playing ' \
                                                                 'with RigD Custom Activities',
             "settings": settings
         }
-        base_url = "https://api.zoom.us/v2/users/%s/meetings"%urllib.parse.quote(API_USER_ID)
+        base_url = "https://api.zoom.us/v2/users/%s/meetings" % urllib.parse.quote(API_USER_ID)
         try:
             res = requests.post(url=base_url, data=json.dumps(req),
-                                headers={'Content-Type':'application/json',
+                                headers={'Content-Type': 'application/json',
                                          'Authorization': 'Bearer %s' % get_jwt_token(20)})
             response = json.loads(res.text)
 
@@ -140,15 +145,17 @@ def lambda_handler(event, context):
         except Exception as ex:
             slack_msg = slack_msg_failure("An exception occurred %s" % ex)
 
-    resdict = {"format":"Slack", "message": slack_msg}
+    resdict = {"format": "Slack", "message": slack_msg}
     return resdict
+
 
 def main():
     """For local testing. Will be ignored by AWS"""
     tevt = {
         "join_before_host": True
     }
-    pp.pprint(lambda_handler(tevt, ""))
+    pp.pprint(lambda_handler(tevt))
+
 
 if __name__ == "__main__":
     main()
