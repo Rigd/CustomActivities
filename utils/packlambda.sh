561cc0856a3a7dc5995d8155d577b76eb8f7d2cd
Mike Dimitroff <mpd@hexia.org>
2018-11-12 11:22:25 -0800

    Added packaging utility.

---
+++ utils/packlambda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+  then
+    echo "usage:  packlambda.sh <lambda_dir>"
+    exit 1
+fi
+tmpdir=`mktemp -d`
+cp -R $1 $tmpdir
+pushd $tmpdir/$1
+pip install -t . -r requirements.txt --ignore-installed
+zip -r $1 .
+pushd
+mv $tmpdir/$1/$1.zip .
+#zip -r $1 $1
+#echo $tmpdir
